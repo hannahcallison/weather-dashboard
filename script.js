17a4f80b64dcbdaec33c4330b9c5b16f415c845e
Hannah Callison <hannahcallison1007@gmail.com>
2022-04-11 16:14:00 -0700

    More JS and Css styling

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ var searchBar = $("#search")
 var form = $("form")
 var searchHistory = $("#searchHistory")
 var displayCurrent = $("#displayCurrent")
-var fiveDay = $("#fiveDay")
+var fiveDay = $("#fiveDayHeader")
+var cardOne = $("#cardOne")
+var cardTwo = $("#cardTwo")
+var cardThree = $("#cardThree")
+var cardFour = $("#cardFour")
+var cardFive = $("#cardFive")
 var apiKey = "f448b616b40e952f739353b6dc35dd99"
 
 // Current Weather Display//
 $("form").submit(function(event){
   event.preventDefault()
   displayCurrent.text("")
-  fiveDay.text("")
+  cardOne.text("")
+  cardTwo.text("")
+  cardThree.text("")
+  cardFour.text("")
+  cardFive.text("")
+  displayCurrent.css("display", "block")
+  fiveDay.css("display", "block")
+  cardOne.css("display", "block")
+  cardTwo.css("display", "block")
+  cardThree.css("display", "block")
+  cardFour.css("display", "block")
+  cardFive.css("display", "block")
   var city = $("input").val()
   currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&Appid=" + apiKey + "&units=imperial";
   function getApi(){
@@ -45,34 +61,39 @@ $("form").submit(function(event){
         console.log(data)
 
         // Day 1//
-        day1 = fiveDay.append("<p>" + data.list[1].dt_text + "<p>")
-        day1Temp = fiveDay.append("<p>" + "Temp: " + data.list[1].main.temp + "°F" + "<p>")
-        day1Wind = fiveDay.append("<p>" + "Wind: " + data.list[1].wind.speed + " MPH" + "<p>")
-        day1Humidity = fiveDay.append("<p>"+ "Humidity: " + data.list[1].main.humidity + "%" + "<p>")
+        day1 = cardOne.append("<p>" + moment().add(1, "d").format("MMM Do, YYYY") + "<p>")
+        cardOne.append(`<img src="https://openweathermap.org/img/wn/${data.list[1].weather[0].icon}.png">`)
+        day1Temp = cardOne.append("<p>" + "Temp: " + data.list[1].main.temp + "°F" + "<p>")
+        day1Wind = cardOne.append("<p>" + "Wind: " + data.list[1].wind.speed + " MPH" + "<p>")
+        day1Humidity = cardOne.append("<p>"+ "Humidity: " + data.list[1].main.humidity + "%" + "<p>")
 
         // Day 2//
-        day2 = fiveDay.append("<p>" + data.list[2].dt_text + "<p>")
-        day2Temp = fiveDay.append("<p>" + "Temp: " + data.list[2].main.temp + "°F" + "<p>")
-        day2Wind = fiveDay.append("<p>" + "Wind: " + data.list[2].wind.speed + " MPH" + "<p>")
-        day2Humidity = fiveDay.append("<p>"+ "Humidity: " + data.list[2].main.humidity + "%" + "<p>")
+        day2 = cardTwo.append("<p>" + moment().add(2, "d").format("MMM Do, YYYY") + "<p>")
+        cardTwo.append(`<img src="https://openweathermap.org/img/wn/${data.list[2].weather[0].icon}.png">`)
+        day2Temp = cardTwo.append("<p>" + "Temp: " + data.list[2].main.temp + "°F" + "<p>")
+        day2Wind = cardTwo.append("<p>" + "Wind: " + data.list[2].wind.speed + " MPH" + "<p>")
+        day2Humidity = cardTwo.append("<p>"+ "Humidity: " + data.list[2].main.humidity + "%" + "<p>")
 
         // Day 3 //
-        day3 = fiveDay.append("<p>" + data.list[3].dt_text + "<p>")
-        day3Temp = fiveDay.append("<p>" + "Temp: " + data.list[3].main.temp + "°F" + "<p>")
-        day3Wind = fiveDay.append("<p>" + "Wind: " + data.list[3].wind.speed + " MPH" + "<p>")
-        day3Humidity = fiveDay.append("<p>"+ "Humidity: " + data.list[3].main.humidity + "%" + "<p>")
+        day3 = cardThree.append("<p>" + moment().add(3, "d").format("MMM Do, YYYY") + "<p>")
+        cardThree.append(`<img src="https://openweathermap.org/img/wn/${data.list[3].weather[0].icon}.png">`)
+        day3Temp = cardThree.append("<p>" + "Temp: " + data.list[3].main.temp + "°F" + "<p>")
+        day3Wind = cardThree.append("<p>" + "Wind: " + data.list[3].wind.speed + " MPH" + "<p>")
+        day3Humidity = cardThree.append("<p>"+ "Humidity: " + data.list[3].main.humidity + "%" + "<p>")
 
         // Day 4 //
-        day4 = fiveDay.append("<p>" + data.list[4].dt_text + "<p>")
-        day4Temp = fiveDay.append("<p>" + "Temp: " + data.list[4].main.temp + "°F" + "<p>")
-        day4Wind = fiveDay.append("<p>" + "Wind: " + data.list[4].wind.speed + " MPH" + "<p>")
-        day4Humidity = fiveDay.append("<p>"+ "Humidity: " + data.list[4].main.humidity + "%" + "<p>")
+        day4 = cardFour.append("<p>" + moment().add(4, "d").format("MMM Do, YYYY") + "<p>")
+        cardFour.append(`<img src="https://openweathermap.org/img/wn/${data.list[4].weather[0].icon}.png">`)
+        day4Temp = cardFour.append("<p>" + "Temp: " + data.list[4].main.temp + "°F" + "<p>")
+        day4Wind = cardFour.append("<p>" + "Wind: " + data.list[4].wind.speed + " MPH" + "<p>")
+        day4Humidity = cardFour.append("<p>"+ "Humidity: " + data.list[4].main.humidity + "%" + "<p>")
 
         // Day 5 // 
-        day5 = fiveDay.append("<p>" + data.list[5].dt_text + "<p>")
-        day5Temp = fiveDay.append("<p>" + "Temp: " + data.list[5].main.temp + "°F" + "<p>")
-        day5Wind = fiveDay.append("<p>" + "Wind: " + data.list[5].wind.speed + " MPH" + "<p>")
-        day5Humidity = fiveDay.append("<p>"+ "Humidity: " + data.list[5].main.humidity + "%" + "<p>")
+        day5 = cardFive.append("<p>" + moment().add(5, "d").format("MMM Do, YYYY") + "<p>")
+        cardFive.append(`<img src="https://openweathermap.org/img/wn/${data.list[5].weather[0].icon}.png">`)
+        day5Temp = cardFive.append("<p>" + "Temp: " + data.list[5].main.temp + "°F" + "<p>")
+        day5Wind = cardFive.append("<p>" + "Wind: " + data.list[5].wind.speed + " MPH" + "<p>")
+        day5Humidity = cardFive.append("<p>"+ "Humidity: " + data.list[5].main.humidity + "%" + "<p>")
       })
     })
   }
@@ -82,13 +103,8 @@ $("form").submit(function(event){
 
 
 
-// Five Day//
 
-
-
-// // Forloop for persisting the data onto HMTL page
 // for (var i = 0; i < localStorage.length; i++) {
-
 //   var city = localStorage.getItem(i);
 //   // console.log(localStorage.getItem("City"));
 //   var cityName = $(".list-group").addClass("list-group-item");
